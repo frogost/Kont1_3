533b7dee6144d00f6f251eb2470a062f6c5ed199
payaln <payaln@mail.ru>
2018-10-05 14:50:48 +0300

    Kont1_1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+    int a;
+    a=3;
+    std::cout << "Первое задание а="<< std::endl;
+    std::cout << a*6 << std::endl;
+    std::cout << "Первое задание а="<< std::endl;
+    std::cout << a-9 << std::endl;
+
+    return 0;
+}
